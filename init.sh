17c0aca1aac246b42885d8716f06e79376ce2100
Hank <xchank@outlook.com>
2020-08-23 23:03:44 -0400

    zsh plugins auto detection and installation

--- init.sh
+++ init.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+if [ -d $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions ]; then
+	echo "zsh-autosuggestions installed, skip"
+else
+	git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+	echo "plugins+=(zsh-autosuggestions)" >> $HOME/.zshrc
+fi
+
+if [ -d $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]; then
+	echo "zsh-syntax-highlighting installed, skip"
+else
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+	echo "plugins+=(zsh-syntax-highlighting)" >> $HOME/.zshrc
+fi
 
 if grep -q "source \$HOME/.hank/shrc" $HOME/.bashrc; then
 	echo "bashrc is linked in ~/.bashrc, skip"
